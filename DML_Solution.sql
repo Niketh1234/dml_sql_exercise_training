4f6e2999cbebe8a00826499e3f69af96e28c1afa
dniketh <dniketh@actalentservices.com>
2024-08-19 14:59:53 +0000

    Solution Updated

--- DML_Solution.sql
+++ DML_Solution.sql
@@ -2,7 +2,7 @@ CREATE TABLE Customer(
 CustomerID INT, 
 CustomerName VARCHAR(20), 
 Email VARCHAR(20), 
-Age tinyint, 
+Age TINYINT, 
 Phone VARCHAR(15)) -- Table Creation
 
 INSERT INTO Customer VALUES(1,'Peter','Peter@gmail.com',28,'234-354-2879') -- Insert a single record
